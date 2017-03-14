d19298b2fd77669152744043d13990561bbeb687
Dylan-LANCE <dylan.blakemore@gmail.com>
2017-03-14 13:11:15 +0200

    Tried to implement function to be called on KeyboardInterrupt; unsuccessful

--- location_p.py
+++ location_p.py
@@ -6,7 +6,6 @@ import shutil
 import multiprocessing
 import time
 from ConfigParser import SafeConfigParser
-
 from sklearn.cluster import DBSCAN
 # custom classes
 from lib import dataset
@@ -14,35 +13,65 @@ from lib import frame
 from lib import lofpy
 from lib import vmptutils as vuti
 
+
+#==============================================================================
+#     This is the method that is called by the Pool object, and
+#     which runs in parallel across the desired number of CPU cores.
+#     It takes one frame of data as a parameter and determines the locations 
+#     of the tracers within that frame
+#==============================================================================
 def locate(frame_data_i):
-    # initialize variables
+    # Create a Frame object from the data
     frame_i = frame.Frame(frame_data_i)
+    # Discretize LOR's and generate Voronoi tessellations to determine the 
+    #   smallest cell for each LOR.
     poi = frame_i.getPointsOfInterest(EPS)
     all_points = frame_i.getPointsAt(poi['ind'])
     all_vols = poi['vol']
+    # Get the average time for the frame
     time_i = frame_i.getFrameTime()
     del frame_i
+    # Perform a Local Outlier Factor analysis on the points of interest
     lof = lofpy.getLOF(K, all_points)
     low_lof = vuti.getLowFraction(lof, LOF_FRAC)
     lof_smoothed = all_points[low_lof,:]
     lof_smoothed_vols = np.array(all_vols)[low_lof]
+    # Clean the data further by discarding the points with large Voronoi cells
     low_vol = vuti.getLowFraction(lof_smoothed_vols, VOL_FRAC)
-    # Actual points used in clustering
     remainders = lof_smoothed[low_vol,:]
+    # Perform DBSCAN clustering
     db = DBSCAN(eps=EPS, min_samples=K).fit(remainders)
     labels = db.labels_
     n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
     locations = np.zeros((n_clusters,4))
+    # Calculate geometric mean of each cluster
     for cluster in range(0, n_clusters):
         cluster_inds = np.where(labels == cluster)[0]
         location = np.sum(remainders[cluster_inds,:], axis=0)/len(cluster_inds)
         locations[cluster,0:3] = location
         locations[cluster,3] = time_i
+    #end if
     return locations
+#end locate() method
 
+#==============================================================================
+#     Called when a thread is created.
+#==============================================================================
 def start_process():
     print 'Starting ', multiprocessing.current_process().name
+#end start_process() method
 
+#==============================================================================
+#     Main method.
+#     Initiates constants used in the process by loading the config.ini file.
+#     Asks for user input, namely for the folder containing the input files,
+#         the folder to which the output will be written, and the number
+#         of tracers used in the experiment.
+#     For each input file, the data is split into frames. The set of frames is
+#         passed to the Pool object to be used as parameters for the locate 
+#         method.
+#     After a file has been processed, the output is written to the output file.
+#==============================================================================
 if __name__ == "__main__":
     ## Constants used throughout the algorithm, imported from the config.ini file ##
     config = SafeConfigParser()
@@ -57,7 +86,6 @@ if __name__ == "__main__":
     LOF_FRAC = config.getfloat('Filter','Lof_Frac')
     VOL_FRAC = config.getfloat('Filter','Vol_Frac')
     NUM_CORES = config.getint('Processing','Num_Cores')
-    ########################
     
     # if the number of cores to use is greater than the number
     # of physical cores (or -1) set to maximum
@@ -85,6 +113,8 @@ if __name__ == "__main__":
                 folder_exists = 0
         else:
             os.makedirs(output_folder)
+        #end if
+    #end while folder_exists
                   
     num_tracers = int(raw_input('Enter the number of tracers expected: '))
     
@@ -101,12 +131,12 @@ if __name__ == "__main__":
     end_file = num_files
     if end_file > num_files:
         end_file = num_files
+    #end if
         
     # calculate the size (number of lines) of each frame
     frame_size = LINES_PER_TRACER * num_tracers
     
     for file_num in range(start_file, end_file):
-    
         try:
             file_path = input_files[file_num]
             print('==================================================')
@@ -126,7 +156,11 @@ if __name__ == "__main__":
                    + file_path + ' in ' + str(end-start) + 's')
             vuti.writeOutputToFile(output_folder, np.vstack(np.array(pool_output)))
         except KeyboardInterrupt, SystemExit:
+            pool.close()
+            pool.join()
             print('\n Operation cancelled, writing data to file...')
+            vuti.writeOutputToFile(output_folder, np.vstack(np.array(pool_output)))
             raise
+        #end try
     #end for file_num
 #end main
